7f2361d60a675aa638a4fe6ab52908a89fbd7e50
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-12-18 17:39:26 +0200

    added quotation marks to the title of cards

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 function Note({title, content, id, name, onDel}) {
   return (
     <div className="note" >
-      <h1>{title}</h1>
+      <h1>"{title}"</h1>
       <p>{content}</p>
       <button onClick={onDel} id={id} name={name}>
         <DeleteIcon />
