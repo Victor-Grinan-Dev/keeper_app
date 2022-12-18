e823d7fe8f254045b8b2a1fa551de7485ab44953
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-12-18 17:30:32 +0200

    deleted instructions

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
-//import Card from './Card'
-//import conctacts from './contacts'
-import notes from './notes'
+
 import Note from './Note'
 
 function Browser() {
@@ -11,10 +9,6 @@ useEffect(() => {
   const data = JSON.parse(localStorage.getItem("notes"));
   setLocalNotes(data);
 }, []);
-
-const hideItem = () => {
-   
-};
 
 const delItem = (e) => {
    const data = JSON.parse(localStorage.getItem('notes'));
@@ -27,7 +21,6 @@ const delItem = (e) => {
 
   return (
     <div className='browser'>
-        {notes.map(note =>  <Note key={note.id} title={note.title} content={note.content} id={note.id} onDel={hideItem}/> )}
         {localNotes && localNotes.map((note,i) => <Note key={i} id={i} name={note.title} title={note.title} content={note.content} onDel={(e) => delItem(e)}/>)}
     </div>
   )
--- src/components/notes.js
+++ src/components/notes.js
@@ -2,7 +2,7 @@ const notes = [
     {
         id:1,
         title:'how to use this app',
-        content:"\n Creating a note: \n 1 - Click in the create note section \n 2 - Fill the title field and the content field \n 3 - Hit the plus button. \n \n Input a new line in the text: \n - write your text as normal and every time you need a new line input the symbol backslash followed by a lowercase n (\\n) \n\n  how to delete a note \n Just hit the good 'ol trash can icon.",
+        content:"\n Creating a note: \n 1 - Click in the create note section \n 2 - Fill the title field and the content field \n 3 - Hit the plus button. \n\n  how to delete a note \n Just hit the good 'ol trash can icon.",
         
     }
 ]
